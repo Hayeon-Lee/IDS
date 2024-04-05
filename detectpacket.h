fd774de5d9a326535a95fe8cdec6c0d30edc20dc
이하연 <papepopepe@naver.com>
2024-04-05 14:27:08 +0900

    Update and rename detect.h to detectpacket.h

--- detectpacket.h
+++ detectpacket.h
@@ -5,15 +5,6 @@
 #include <pcap.h>
 #include <netinet/in.h>
 
-typedef struct {
-  struct ether_header *ethernet_header;
-  struct in_addr srcip;
-  struct in_addr dstip;
-  unsigned short protocol;
-  unsigned short srcport;
-  unsigned short dstport;
-} Packet;
-
 void makePacketNode(const u_char *packet, struct pcap_pkthdr *header);
 unsigned short readEthernet(const u_char *packet, Packet *packet_node);
 int readIPV4(const u_char *packet, Packet *packet_node);
