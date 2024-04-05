cb416f9a54dc32cf11d7811f687d9a0da2d746ea
이하연 <papepopepe@naver.com>
2024-04-05 11:57:09 +0900

    Update queue.h

--- queue.h
+++ queue.h
@@ -7,6 +7,9 @@
 #define MAC_ADDR_LEN 18 
 #define IP_ADDR_LEN 16
 #define PAYLOAD_LEN 1461
+
+#define RULE_NAME_LEN 16
+
 #include <pcap.h>
 
 //Circular Queue Structure
@@ -23,9 +26,7 @@ typedef struct {
   const u_char *packet[MAX_QUEUE_SIZE]; 
 } PacketQueue;
 
-/*
-//DangerPacketQueue에 들어갈 아이템(얘가 큐가 아님)
-//DangerPacketQueue Item Structure
+//DangerPacketItem
 typedef struct {
   unsigned long long detectiontime;
   unsigned short srcport;
@@ -35,7 +36,14 @@ typedef struct {
   unsigned char dstip[IP_ADDR_LEN];
   unsigned char rulename[RULE_NAME_LEN];
 } DangerPacket; 
-*/
+
+//DangerPacketQueue
+typedef struct {
+  int front, rear;
+  int count;
+  DangerPacket items[MAX_QUEUE_SIZE];
+} DangerPacketQueue;
+
 void initQueue(CircularQueue *queue);
 void enqueue(CircularQueue *queue, int value);
 void dequeue(CircularQueue *queue);
@@ -43,5 +51,10 @@ void dequeue(CircularQueue *queue);
 void initPacketQueue(PacketQueue *queue);
 void enqueuePacket(PacketQueue *queue, const u_char *value, int size);
 void dequeuePacket(PacketQueue *queue);
+
+void initDangerPacketQueue(DangerPacketQueue *queue);
+void enqueueDangerPacket(DangerPacketQueue *queue, DangerPacket value);
+void dequeueDangerPacket(DangerPacketQueue *queue);
+
 
 #endif
