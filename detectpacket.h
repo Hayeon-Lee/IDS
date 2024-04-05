45957732f8765a13f0d4ea7cfd208fe2dc303f71
이하연 <papepopepe@naver.com>
2024-04-05 15:22:49 +0900

    Update detectpacket.h

--- detectpacket.h
+++ detectpacket.h
@@ -5,6 +5,7 @@
 #include <pcap.h>
 #include <netinet/in.h>
 
+int startDetectThread(void *detectstruct);
 void makePacketNode(const u_char *packet, struct pcap_pkthdr *header);
 unsigned short readEthernet(const u_char *packet, Packet *packet_node);
 int readIPV4(const u_char *packet, Packet *packet_node);
