2da6aad8ddcf1fec6d2e6a7393eca97a373c530e
이하연 <papepopepe@naver.com>
2024-04-07 19:11:41 +0900

    Update queue.c

--- queue.c
+++ queue.c
@@ -37,28 +37,37 @@ void initPacketQueue(PacketQueue *queue) {
   queue->front = 0;
   queue->rear = -1;
   queue->count = 0;
+  pthread_mutex_init(&(queue->mutex), NULL);
 }
 
 void enqueuePacket(PacketQueue *queue, Packet *value, int size){
+  pthread_mutex_lock(&(queue->mutex));
 
   if (queue->count >= MAX_QUEUE_SIZE) {
     printf("Queue가 꽉 차 드롭합니다.\n");
+    pthread_mutex_unlock(&(queue->mutex));
     return;
   }
 
   queue->rear = ((queue->rear)+1)%MAX_QUEUE_SIZE;
   queue->packet[queue->rear] = value;
-   queue->count += 1;
+  queue->count += 1;
+  pthread_mutex_unlock(&(queue->mutex));
 }
 
 void dequeuePacket(PacketQueue *queue){
+  
+  pthread_mutex_lock(&(queue->mutex));  
+
   if (queue->count <= 0) {
     printf("Queue가 비어있습니다.");
+    pthread_mutex_unlock(&(queue->mutex));
     return;
   }
   
   queue->front = ((queue->front)+1)%MAX_QUEUE_SIZE;
   queue->count -= 1;
+  pthread_mutex_unlock(&(queue->mutex));
 }
 
 void initDangerPacketQueue(DangerPacketQueue *queue) {
