1740ef0c4fa54ad6f1f382ea5337457c41bb2994
이하연 <papepopepe@naver.com>
2024-04-09 12:04:52 +0900

    Update detectpacket.h

--- detectpacket.h
+++ detectpacket.h
@@ -2,13 +2,34 @@
 #define DETECTPACKET_H
 
 #include <netinet/if_ether.h>
+#include <netinet/ether.h>
 #include <pcap.h>
 #include <netinet/in.h>
 
-int startDetectThread(void *detectstruct);
-void makePacketNode(const u_char *packet, struct pcap_pkthdr *header);
-unsigned short readEthernet(const u_char *packet, Packet *packet_node);
-int readIPV4(const u_char *packet, Packet *packet_node);
-int readUDP (const u_char *packet, int ipsize, Packet *packet_node);
+typedef struct {
+  int length;
 
+  unsigned short dstmac[ETH_ALEN];
+  unsigned short srcmac[ETH_ALEN];
+  unsigned short ethertype;
+
+  unsigned int srcip; //host to network 변환됨
+  unsigned int dstip; //host to network 변환됨
+  unsigned short protocol;
+
+  unsigned short srcport; //host to network 변환됨
+  unsigned short dstport; //host to network 변환됨
+
+  short flag_payload;
+  int size_payload;
+  char payload[1461];
+} PacketNode;
+
+void initPacketNode(PacketNode *node);
+int startDetectThread(void * detectstruct);
+PacketNode makePacketNode(u_char *packet, int caplen);
+unsigned short readEthernet(u_char *packet, PacketNode *node);
+int readIPV4(u_char *packet, PacketNode *node);
+int readUDP (u_char *packet, PacketNode *node);
+int readTCP(u_char *packet, PacketNode *node);
 #endif
