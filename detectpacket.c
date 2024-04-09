a27a53d72a8129fdea79f2e92db210675d6292fa
이하연 <papepopepe@naver.com>
2024-04-09 12:04:32 +0900

    Update detectpacket.c

--- detectpacket.c
+++ detectpacket.c
@@ -1,89 +1,136 @@
 #include <netinet/udp.h>
+#include <netinet/tcp.h>
 #include <netinet/ip.h>
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "queue.h"
 #include "detectpacket.h"
 
-int startDetectThread(void *detectstruct);
-unsigned short readEthernet(const u_char *packet, Packet *packet_node);
-int readIPV4(const u_char *packet, Packet *packet_node);
-int readUDP (const u_char *packet, int ipsize, Packet *packet_node);
 
-int startDetectThread(void * detectstruct) {
-  DetectStruct *tmpstruct = (DetectStruct *)detectstruct;
-  PacketQueue *tmpqueue = tmpstruct->packetqueue;
-  
+int startDetectThread(void * detectstruct) { 
+  PacketQueue *pkt_queue = ((DetectStruct *)detectstruct)->packetqueue;
+  Rule rulestruct = ((DetectStruct *)detectstruct)->rulestruct;
+
   while(1){
     sleep(1);
 
-    Packet * item = dequeuePacket(tmpqueue);
+    Packet *item = dequeuePacket(pkt_queue);
 
     if (item) {
-        printf("dequeue: %d\n", tmpqueue->count);
-        makePacketNode(item->packet, item->header);
+      PacketNode node;
+      node = makePacketNode(item->packet, item->caplen);
+      printf("%d\n", node.protocol);
     }
   }
 }
 
-void makePacketNode (const u_char *packet, struct pcap_pkthdr *header) { 
-  Packet packet_node;
-  printf("%d\n", header->caplen);
-  if (header->caplen >= 14) {
-    //이더넷 헤더
-    unsigned short type = readEthernet(packet, &packet_node);
-    
+void initPacketNode(PacketNode *node){
+  node->length = -1;
+  
+  node->ethertype = -1;
+
+  node->srcip = -1;
+  node->dstip = -1;
+  node->protocol = -1;
+
+  node->srcport = -1;
+  node->dstport = -1;
+
+  //payload 유무, payload 사이즈
+  node->flag_payload = 0;
+  node->size_payload = 0;
+}
+
+PacketNode makePacketNode (u_char *packet, int caplen) { 
+  PacketNode node;
+  initPacketNode(&node);
+  
+  //이더넷 헤더일 경우
+  if (caplen >= 14) {
+    //node 길이 초기화
+    node.length = caplen;
+    unsigned short type = readEthernet(packet, &node);
+
     //IPV4
-    if (type == ETHERTYPE_IP && header->caplen >= 34) {
-      int protocol = readIPV4(packet, &packet_node);
-      
+    if (type == ETHERTYPE_IP && caplen >= 34) {
+      int protocol = readIPV4(packet, &node);
+
       //TCP
-      if (protocol == 6 && header->caplen >= 54) {
+      if (protocol == 6 && caplen >= 54) {
+        readTCP(packet, &node);
         printf("tcp입니다\n");
       }
       //UDP
-      if (protocol == 17 && header->caplen >= 42) {
-     //   readUDP(packet, 20, &packet_node);
-        //printf("%u\n", packet_node.dstport);
-        printf("udp입니다\n");
+      if (protocol == 17 && caplen >= 42) {
+        readUDP(packet, &node);
+        printf("udp입니다\n");  
       }
       //ICMP
-      if (protocol == 1 && header->caplen >= 42) {
+      if (protocol == 1 && caplen >= 42) {
         printf("icmp입니다.\n");
       }
     }
   }
+
+  return node;
 }
 
-unsigned short readEthernet(const u_char *packet, Packet *packet_node) {
+unsigned short readEthernet(u_char *packet, PacketNode *node ) {
 
-    struct ether_header *eth_header;
-    eth_header = (struct ether_header*)packet;
+  struct ether_header *eth_header;
+  eth_header = (struct ether_header*)packet;
 
-    //packet_node->ethernet_header = eth_header;
+  node->ethertype = ntohs(eth_header->ether_type);
+  memcpy(node->dstmac, eth_header->ether_dhost, ETH_ALEN);
+  memcpy(node->srcmac, eth_header->ether_shost, ETH_ALEN);
 
-    //return ntohs(packet_node->ethernet_header->ether_type);
+  return ntohs(eth_header->ether_type);
 }
 
-int readIPV4(const u_char *packet, Packet * packet_node) {
-
-    struct ip *ip_header = (struct ip*)(packet+sizeof(struct ether_header));
- //   packet_node->srcip = ip_header->ip_src;
-  //  packet_node->dstip = ip_header->ip_dst;
-  //  packet_node->protocol = ip_header->ip_p;
-
- //   return packet_node->protocol;
+int readIPV4(u_char *packet, PacketNode *node) {
+  struct ip *ip_header = (struct ip*)(packet+sizeof(struct ether_header));
+  node->srcip = htonl(ip_header->ip_src.s_addr);
+  node->dstip = htonl(ip_header->ip_dst.s_addr);
+  node->protocol = ip_header->ip_p;
+  return ip_header->ip_p;
 }
 
-int readUDP(const u_char *packet, int ipsize, Packet *packet_node) {
-    int ether_size = sizeof(struct ether_header);
-    int add_size = ether_size + ipsize;
+int readUDP(u_char *packet, PacketNode *node) {
+  int ether_size = sizeof(struct ether_header);
+  int add_size = ether_size + 20;
 
-    struct udphdr *udp_header = (struct udphdr*)(packet + add_size); 
+  struct udphdr *udp_header = (struct udphdr*)(packet + add_size); 
+  node->srcport = udp_header->uh_sport;
+  node->dstport = udp_header->uh_dport;
+
+  if (node->length > 42) {
+    node->flag_payload = 1;
+    int payload_size = (node->length) - 42; 
+    node->size_payload = payload_size;
+    memcpy(node->payload, (packet+41), payload_size); 
+  }
+
+  return 0;
+}
+
+int readTCP(u_char *packet, PacketNode *node) {
+  int ether_size = sizeof(struct ether_header);
+  int add_size = ether_size + 20;
+
+  struct tcphdr *tcp_header = (struct tcphdr*)(packet+add_size);
+
+  node->srcport = htons(tcp_header->th_sport);
+  node->dstport = htons(tcp_header->th_dport);
   
- //   packet_node->srcport = udp_header->uh_sport;
-  //  packet_node->dstport = udp_header->uh_dport;
-    return 0;
+  if (tcp_header->th_flags & TH_PUSH) {
+    node->flag_payload = 1;
+    int payload_size = (node->length) - 54;
+    node->size_payload = payload_size;
+    memcpy(node->payload, (packet+53), payload_size);
+  }    
+
+  return 0;
 }
