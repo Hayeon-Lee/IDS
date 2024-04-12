f7fb3a3c5ea80a0ada665ac8e9fc2c972c4d8ab2
이하연 <papepopepe@naver.com>
2024-04-12 15:42:44 +0900

    Update queue.c

--- queue.c
+++ queue.c
@@ -4,36 +4,6 @@
 #include "queue.h"
 #include "detectpacket.h"
 
-void initQueue(CircularQueue *queue) {
-  queue->front = 0;
-  queue->rear = -1;
-  queue->count = 0;
-  
-  return;
-}
-
-void enqueue(CircularQueue *queue, int value) {
-  if (queue->count >= MAX_QUEUE_SIZE) {
-    printf("Queue가 꽉 차 대기합니다.");
-    return;
-  }
-
-  queue->rear = ((queue->rear)+1)%MAX_QUEUE_SIZE;
-  queue->data[queue->rear] = value;
-  queue->count += 1;
-}
-
-void dequeue(CircularQueue *queue) {
-  if (queue->count <=0) {
-    printf("Queue가 비어있습니다.");
-    return;
-  }
-
-  //int value = queue->data[queue->front];
-  queue->front = ((queue->front)+1)%MAX_QUEUE_SIZE;
-  queue->count -= 1;
-}
-
 void initPacketQueue(PacketQueue *queue) {
   queue->front = 0;
   queue->rear = -1;
@@ -41,19 +11,19 @@ void initPacketQueue(PacketQueue *queue) {
   pthread_mutex_init(&(queue->mutex), NULL);
 }
 
-void enqueuePacket(PacketQueue *queue, Packet *value, int size){
+int enqueuePacket(PacketQueue *queue, Packet *value, int size){
   pthread_mutex_lock(&(queue->mutex));
 
   if (queue->count >= MAX_QUEUE_SIZE) {
-    printf("Queue가 꽉 차 드롭합니다.\n");
     pthread_mutex_unlock(&(queue->mutex));
-    return;
+    return -1;
   }
   
   queue->rear = ((queue->rear)+1)%MAX_QUEUE_SIZE;
   queue->packet[queue->rear] = value;
   queue->count += 1;
   pthread_mutex_unlock(&(queue->mutex));
+  return 1;
 }
 
 Packet * dequeuePacket(PacketQueue *queue){
@@ -61,7 +31,6 @@ Packet * dequeuePacket(PacketQueue *queue){
   pthread_mutex_lock(&(queue->mutex));  
 
   if (queue->count <= 0) {
-    printf("Queue가 비어있습니다.");
     pthread_mutex_unlock(&(queue->mutex));
     return NULL;
   }
@@ -86,7 +55,6 @@ void enqueueDangerPacket(DangerPacketQueue *queue,DangerPacket *value) {
   pthread_mutex_lock(&(queue->mutex));
 
   if (queue->count >= MAX_QUEUE_SIZE) {
-    printf("DangerQueue가 꽉 차 드롭합니다.\n");
     pthread_mutex_unlock(&(queue->mutex));
     return;
   } 
@@ -101,7 +69,6 @@ DangerPacket * dequeueDangerPacket(DangerPacketQueue *queue) {
   pthread_mutex_lock(&(queue->mutex));
 
   if(queue->count <= 0) {
-//    printf("DangerQueue가 비어있습니다.\n");
     pthread_mutex_unlock(&(queue->mutex));
     return NULL;
   }
