ae39464a4f43bd3866f9834b0c8d37e473074cfc
이하연 <papepopepe@naver.com>
2024-04-26 11:11:52 +0900

    Update readpacket.h

--- readpacket.h
+++ readpacket.h
@@ -3,10 +3,13 @@
 
 #define MAX_FILENAME_LEN 511 //255+255+1(/)+1('\0')
 
+#include <pcap.h>
 #include <dirent.h>
 #include "queue.h"
 
 void *start_readthread(void *readstruct);
-int check_extension(const char *filename);
-void accessPacketFiles(DIR *directory, char * directory_path, PacketQueue* *packetqueue_array, DangerPacketQueue *dangerpacketqueue, int threadcnt);
+int check_filename_extension(const char *filename);
+void read_packet_files(DIR *directory, char * directory_path, PacketQueue **packetqueue_array, DangerPacketQueue *dangerpacketqueue, int threadcnt, struct dirent *entry);
+DangerPacket *make_danger_packet_node();
+Packet *make_packet_node(struct pcap_pkthdr *header, const u_char *packet);
 #endif
