8b9ee7e4b01b5d26ae8f8be863fde0ad18773291
이하연 <papepopepe@naver.com>
2024-04-29 15:35:10 +0900

    Update hashtable.c

--- hashtable.c
+++ hashtable.c
@@ -94,16 +94,18 @@ HashTableNode* make_table_node(unsigned int srcip) {
   return node;
 }
 
+//함수의 목적이 2개가 되기 때문이다
 HashTableNode* find_target_location(HashTable *hashtable, int key, unsigned srcip){
   HashTableNode *node = hashtable->node[key].next;
 
   while(node != NULL) {
     if (node->srcip == srcip) return node; //그 아이피를 가진 노드를 반환해준다
     if (node->next == NULL) return node; //다음 위치가 null이면 (끝까지 찾은거니까) 이 아이피는 없는 아이피이므로 반환한다.
+    //마지막 노드의 위치를 따로 저장을 해둔 다음 그 위치에 붙이는 것이 좋다 (가독성 측면에서) 
+    node = node->next;
   }
 
-  node = NULL;
-  return node;
+  return NULL; //오류가 발생할 수 있는 부분이기 때문에 명시적으로 NULL 을 리턴해주는 것이 좋다.
 }
 
 int insert_table_node(HashTable *hashtable, int key, unsigned int srcip, int isEmpty){
@@ -115,6 +117,9 @@ int insert_table_node(HashTable *hashtable, int key, unsigned int srcip, int isE
     return SUCCESS; 
   }
 
+/*
+위치를 저장한 뒤 그 부분에 추가하는 것으로 바꾸는 것이 좋을 것 같습니다
+*/
 
   struct timespec detect_ts, current_ts;
   clock_gettime(CLOCK_REALTIME, &current_ts);
@@ -178,7 +183,13 @@ int check_table(HashTable *hashtable, unsigned int srcip) {
   pthread_mutex_lock(&(hashtable->mutex));
   
   uint32_t key = hash_src_ip(srcip) % (hashtable->tablesize);
-  int isEmpty = is_empty_hash_table(hashtable, key);
+  int isEmpty = is_empty_hash_table(hashtable, key); //함수로 만들지 않아도 되지 않았을까? - inline으로 짜는 게 어땠을까? - 성능적 측면에서 나쁘지 않을까?
+ 
+/* 
+* hashtable 자료구조랑 프로그램의 로직이 섞여 불편한 것 같습니다: 코드 재활용이 어려워보입니다 
+* 해시테이블 키 별로 들어갈 수 있는 항목의 제한이 있으면 좋을 것 같습니다: 해시테이블이 저장할 수 있는 노드의 개수를 제한하면 좋겠습니다 
+* create/destroy/insert/remove/count/get... -> 라이브러리 개발 시 함수의 이름을 정형화시키는 것이 좋을 것 같습니다 (확장성과 범용성을 고려했을 때)
+*/
 
   int insertResult = 0;
   insertResult = insert_table_node(hashtable, key, srcip, isEmpty);  
